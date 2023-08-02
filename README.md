d1fb659bab66860cd1e8e6e8e9c2c8f7d944a751
mishra85abhijeet <mishra85abhijeet@gmail.com>
2023-08-02 06:32:48 +0530

    "final commit"

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # mlflow_26Jul
+
+MLFLOW_TRACKING_URI=https://dagshub.com/mishra85abhijeet/mlflow_26Jul.mlflow \
+MLFLOW_TRACKING_USERNAME=mishra85abhijeet \
+MLFLOW_TRACKING_PASSWORD=286134796b11c6badc3f44ce0c808a44561bfc31 \
+python script.py
+
+
+'''bash
+
+export MLFLOW_TRACKING_URI=https://dagshub.com/mishra85abhijeet/mlflow_26Jul.mlflow
+
+export MLFLOW_TRACKING_USERNAME=mishra85abhijeet
+
+export MLFLOW_TRACKING_PASSWORD=286134796b11c6badc3f44ce0c808a44561bfc31
+
+'''
--- main.py
+++ main.py
@@ -71,11 +71,14 @@ if __name__ == "__main__":
         mlflow.log_metric("r2", r2)
         mlflow.log_metric("mae", mae)
 
-        predictions = lr.predict(train_x)
-        signature = infer_signature(train_x, predictions)
-
+        
+        
+        ## for remote server
+        remote_server_uri = "https://dagshub.com/mishra85abhijeet/mlflow_26Jul.mlflow"
+        mlflow.set_tracking_uri(remote_server_uri)
+        
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
-
+        
         # Model registry does not work with file store
         if tracking_url_type_store != "file":
             # Register the model
@@ -83,7 +86,6 @@ if __name__ == "__main__":
             # please refer to the doc for more information:
             # https://mlflow.org/docs/latest/model-registry.html#api-workflow
             mlflow.sklearn.log_model(
-                lr, "model", registered_model_name="ElasticnetWineModel", signature=signature
-            )
+                lr, "model", registered_model_name="ElasticnetWineModel")
         else:
             mlflow.sklearn.log_model(lr, "model", signature=signature)
